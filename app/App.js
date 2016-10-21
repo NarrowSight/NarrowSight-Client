a29f077b05d3b126f8a044fec3103cc6a5c08983
Mark de Jong <mark.jong.de@gmail.com>
2016-10-21 10:43:06 +0200

    Setting up Yarn, React, Webpack, Babel including a Hello World component (#1)
    * Setting up Yarn, React, Webpack, Babel including a Hello World component
    
    * make the use of let consistant

---
+++ app/App.js
@@ -0,0 +1,11 @@
+let React = require('react');
+
+let HelloWorld = React.createClass({
+    render: function() {
+        return (
+            <h1>Ja hallo wereld!</h1>
+        );
+    }
+});
+
+module.exports = HelloWorld;
---
+++ app/components/App.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var HelloWorld = React.createClass({
+    render: function() {
+        return (
+            <h1>Ja hallo wereld!</h1>
+        );
+    }
+});
+
+module.exports = HelloWorld;
